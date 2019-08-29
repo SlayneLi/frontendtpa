fcdfe373358e6f54c004fa271a19f89e87afc935
SlayneLi <leonardisteven999@gmail.com>
2019-08-29 23:30:39 +0700

    added footer usable links

--- src/shared/footer/footer.tsx
+++ src/shared/footer/footer.tsx
@@ -16,13 +16,13 @@ export default class FooterComponent extends React.Component{
                         </div>
                         <br/>
                         <div className="link-section">
-                            <a href="#">Careers</a>
-                            <a href="#">News</a>
-                            <a href="#">Policies</a>
-                            <a href="#">Help</a>
-                            <a href="#">Diversity &amp; Belonging</a>
+                            <a href="https://careers.airbnb.com/">Careers</a>
+                            <a href="https://press.airbnb.com">News</a>
+                            <a href="https://www.airbnb.com/help/topic/250/terms-policies">Policies</a>
+                            <a href="https://www.airbnb.com/help?from=footer">Help</a>
+                            <a href="https://www.airbnb.com/diversity">Diversity &amp; Belonging</a>
                             <div className="with-box">
-                                <a href="#">Accessibility</a>
+                                <a href="https://www.airbnb.com/d/accessibility">Accessibility</a>
                                 <div className="i-block">New</div> 
                             </div>
                         </div>
@@ -33,16 +33,16 @@ export default class FooterComponent extends React.Component{
                         </div>
                         <br/>
                         <div className="link-section">
-                            <a href="#">Trust &amp; Safety</a>
-                            <a href="#">Travel Credit</a>
-                            <a href="#">Gift Cards</a>
-                            <a href="#">Airbnb Citizen</a>
-                            <a href="#">Business Travel</a>
+                            <a href="https://www.airbnb.com/trust">Trust &amp; Safety</a>
+                            <a href="https://www.airbnb.com/invite?r=6">Travel Credit</a>
+                            <a href="https://www.airbnb.com/gift?s=footer">Gift Cards</a>
+                            <a href="https://www.airbnbcitizen.com/?utm_source=airbnb&utm_medium=footer&utm_campaign=product">Airbnb Citizen</a>
+                            <a href="https://www.airbnbforwork.com/?utm_source=airbnb&utm_medium=footer&utm_campaign=product">Business Travel</a>
                             <div className="with-box">
-                                <a href="#">Things To Do</a>
+                                <a href="https://www.airbnb.com/things-to-do">Things To Do</a>
                                 <div className="i-block">New</div> 
                             </div>
-                            <a href="#">Airbnbmag</a>
+                            <a href="https://www.airbnb.com/magazine">Airbnbmag</a>
                         </div>
                     </div>
                     <div className="links-vertical">
@@ -51,28 +51,28 @@ export default class FooterComponent extends React.Component{
                         </div>
                         <br/>
                         <div className="link-section">
-                            <a href="#">Why Host</a>
-                            <a href="#">Hospitality</a>
-                            <a href="#">Responsible Hosting</a>
-                            <a href="#">Community Center</a>
+                            <a href="https://www.airbnb.com/host/homes?from_footer=1">Why Host</a>
+                            <a href="https://www.airbnb.com/hospitality">Hospitality</a>
+                            <a href="https://www.airbnb.com/help/article/1376/responsible-hosting-in-the-united-states">Responsible Hosting</a>
+                            <a href="https://community.withairbnb.com/t5/Community-Center/ct-p/community-center">Community Center</a>
                             <div className="with-exp">
-                                <a href="#">Host an Experience</a>
+                                <a href="https://www.airbnb.com/host/experiences?from_footer=1">Host an Experience</a>
                                 <div className="i-block">New</div> 
                             </div>
-                            <a href="#">Open Homes</a>
+                            <a href="https://www.airbnb.com/openhomes?from_footer=1">Open Homes</a>
                         </div>
                     </div>
                     <div className="links-vertical">
                         <div className="title-section logo">
-                            <img src={facebookImg} alt="facebook-svg"/>
-                            <img src={twitterImg} alt="twitter-svg"/>
-                            <img src={instagramImg} alt="instagram-svg"/>
+                            <a href="https://www.facebook.com/airbnb"><img src={facebookImg} alt="facebook-svg"/></a>
+                            <a href="https://twitter.com/airbnb"><img src={twitterImg} alt="twitter-svg"/></a>
+                            <a href="https://www.instagram.com/airbnb/"><img src={instagramImg} alt="instagram-svg"/></a>
                         </div>
                         <br/>
                         <div className="link-section">
-                            <a href="#">Terms</a>
-                            <a href="#">Privacy</a>
-                            <a href="#">Site Map</a>
+                            <a href="https://www.airbnb.com/terms">Terms</a>
+                            <a href="https://www.airbnb.com/terms/privacy_policy">Privacy</a>
+                            <a href="https://www.airbnb.com/sitemaps/v2">Site Map</a>
                         </div>
                     </div>
                 </div>
