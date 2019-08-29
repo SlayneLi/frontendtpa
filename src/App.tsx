0338e6c49909335236f23c5ada82c0fb4a954bb5
rvreich <vintzclagoz@gmail.com>
2019-08-29 22:09:52 +0700

    added home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import HeaderComponent from './shared/header/header';
+import HomeComponent from './shared/pages/home/home';
 import FooterComponent from './shared/footer/footer';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <HeaderComponent />
+      <HomeComponent />
       <FooterComponent />
     </div>
   );
--- src/shared/pages/home/home.tsx
+++ src/shared/pages/home/home.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import './home.scss';
 
-export default class HomePage extends React.Component{
-    
+export default class HomeComponent extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div className="quickcard-section">
+                    <div className="quickcard-box">
+                        <div className="title">
+                            <h1>Book unique places to stay and things to do.</h1>
+                        </div>
+                        <div className="search-content">
+                            <label htmlFor="">WHERE</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="explore-section">
+
+                </div>
+                <div className="place-around-section">
+
+                </div>
+                <div className="experience-section">
+
+                </div>
+            </React.Fragment>
+        );
+    }
 }
