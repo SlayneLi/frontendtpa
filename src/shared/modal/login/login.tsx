b7dcf372a73e754d92baadd0fc00539b5dc9237a
SlayneLi <leonardisteven999@gmail.com>
2019-08-29 10:48:08 +0700

    removed Formik

--- src/shared/modal/login/login.tsx
+++ src/shared/modal/login/login.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 import close from "../../Images/close.svg";
+import facebook from "../../Images/fbBlue.svg";
+import google from "../../Images/GoogleLogo.svg";
+import email from "../../Images/email.svg";
+import eye from "../../Images/eye.svg";
+import lock from "../../Images/lock.svg";
 import "./login.scss";
-import { LoginView } from './view/LoginView';
 
 export default class LoginModal extends React.Component<any,any>{
     constructor(props:any){
@@ -41,7 +45,46 @@ export default class LoginModal extends React.Component<any,any>{
                     isOpen={this.modalState()}
                 >
                     <img src={close} alt="close logo" onClick={this.closeModal} className="closeLogo"/>                    
-                    <LoginView submit={this.dummySubmit}/>
+                    <form className="login">
+                    <div className="fbLogin">
+                        <img src={facebook} alt="fb logo" className="fbLogo"/>
+                        <div>Login with Facebook</div>
+                    </div>
+                    <div className="googleLogin">
+                        <img src={google} alt="google logo" className="gLogo"/>
+                        <div>Login with Google</div>
+                    </div>
+                    <div className="divider">
+                        <hr/>
+                        <div>Or</div>
+                        <hr/>
+                    </div>
+                    <div className="emailBar" >
+                        <input type="text" name="email" placeholder="Email"/>
+                        <img src={email} alt="emailLogo"/>
+                    </div>
+                    <div className="passBar">
+                        <input type="password" name="pass" placeholder="Password"/>
+                        <div>
+                            <img src={eye} alt="eyeLogo"/>
+                            <img src={lock} alt="lockLogo"/>
+                        </div>
+                    </div>
+                    <div className="remMe">
+                        <input type="checkbox" name="remMe"/>
+                        <div>Remember me</div>
+                    </div>
+                    <div className="loginBtn">
+                        <button type="submit">Login</button>
+                    </div>
+                    <div>
+                        <div>Forget Password</div>
+                    </div>
+                    <div>
+                        <div>Don't have an account?</div>
+                        <div>Sign Up</div>
+                    </div>
+                </form>
                 </ReactModal>
             </div>
         );
