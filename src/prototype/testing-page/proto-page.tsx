f5bf991438e45947489fa69b732c5a20d5b683a7
rvreich <vintzclagoz@gmail.com>
2019-09-17 14:15:25 +0700

    added profile content for update purpose

--- src/prototype/testing-page/proto-page.tsx
+++ src/prototype/testing-page/proto-page.tsx
@@ -3,6 +3,7 @@ import Stepper from '../stepper/horizontal-stepper'
 import RxJsInput from '../observable/rxjs-input'
 import ImageUploader from '../image-upload/image-upload'
 import FacebookShare from '../shareable-link/facebook-share'
+import Profile from '../../shared/pages/profile/profile'
 import Axios from 'axios'
 
 export default class ProtoPage extends Component {
@@ -17,6 +18,8 @@ export default class ProtoPage extends Component {
     render() {
         return (
             <div>
+                <Profile id={1} />
+                <br/>
                 <FacebookShare link="http://127.0.0.1:3000/experience/5d727c60a22d2ae0c9c36f67" />
                 {/* <ImageUploader /> */}
                 This is a Testing Prototype Page
