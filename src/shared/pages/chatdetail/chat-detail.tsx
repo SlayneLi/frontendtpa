b60a5a7cd243a19dd9dfb90ad4472186c7caeb55
rvreich <vintzclagoz@gmail.com>
2019-09-18 21:18:00 +0700

    updated chat-detail layout

--- src/shared/pages/chatdetail/chat-detail.tsx
+++ src/shared/pages/chatdetail/chat-detail.tsx
@@ -30,19 +30,91 @@ export default class ChatDetail extends Component {
                         </div>
                         <div className="check-in-out-font">
                             <div className="check-in-title">
-                                Check in
+                                <div>
+                                    Check in
+                                </div>
+                                <div className="date-time">
+                                    May 31, 2019
+                                </div>
                             </div>
                             <div className="to-symbol">
                                 &rarr;
                             </div>
                             <div className="check-out-title">
-                                Check out
+                                <div>
+                                    Check out
+                                </div>
+                                <div className="date-time">
+                                    Jun 6, 2019
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="payment-detail">
+                        <div className="guest-detail">
+                            <div className="guest-title">
+                                Guest
+                            </div>
+                            <div className="guest-count">
+                                6 adults
+                            </div>
+                        </div>
+                        <div className="confirmation-detail">
+                            <div className="confirmation-title">
+                                Confirmation code
+                            </div>
+                            <div className="confirmation-code">
+                                AC8XFE
+                            </div>
+                        </div>
+                        <div className="total-payment-detail">
+                            <div className="payment-title">
+                                Payment
+                            </div>
+                            <div className="night-fee-section">
+                                <div className="nightfee">
+                                    67 x 6 nights
+                                </div>
+                                <div className="total-fee">
+                                    402
+                                </div>
+                            </div>
+                            <div className="cleaning-fee-section">
+                                <div className="cleaning-title">
+                                    Cleaning fee
+                                </div>
+                                <div className="total-cleaning">
+                                    25
+                                </div>
+                            </div>
+                            <div className="service-fee-section">
+                                <div className="service-title">
+                                    Service fee
+                                </div>
+                                <div className="total-service">
+                                    0
+                                </div>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="final-fee">
+                            <div className="final-title">
+                                Total
+                            </div>
+                            <div className="final-amount">
+                                427
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="chat-detail">
-                    DSA
+                    <div className="conversation-title">
+                        CHAT PAGE
+                    </div>
+                    <div className="conversation-box">
+                        CHAT BOX
+                    </div>
                 </div>
             </div>
         )
