e895f863cb1a7062c712cea68c057549a992f32c
rvreich <vintzclagoz@gmail.com>
2019-09-19 14:51:02 +0700

    added basic list for public and self save plan section

---
+++ src/shared/pages/saveplanDetail/save-plan.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './save-plan.scss'
+
+export default class SavePlanList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
